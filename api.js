5fe136eb8919234d982bbf51311d3824b1d496b7
Cameron McEwing <cameronm@missionreadyhq.com>
2024-11-25 10:56:10 +1300

    add api test 02

---
+++ api.js
@@ -0,0 +1,6 @@
+function createUser(user) {
+  // Implementation to be added
+  return user;
+}
+module.exports = { createUser };
+
---
+++ api.test.js
@@ -0,0 +1,11 @@
+const { createUser } = require('./api');  // Import createUser from api.js
+
+describe('User API Tests', () => {
+  test('should create a new user', () => {
+    const newUser = { id: 1, name: 'John Doe' };
+    const result = createUser(newUser); // Function to be implemented
+    expect(result).toEqual(newUser);
+   });
+  test.todo('should fetch a user by ID');
+  test.todo('should delete a user');
+});
